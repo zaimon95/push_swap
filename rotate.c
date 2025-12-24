bc11775ac080647086cce04b4adab43d217515d4
Simon La Grange <sla-gran@fu-r6-p3.s19.be>
2025-12-24 11:44:21 +0100

    feat:rotate

---
+++ rotate.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 11:40:42 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/12/24 11:40:42 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ra(t_list **stack_a)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	first = *stack_a;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	*stack_a = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_list **stack_b)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	first = *stack_b;
+	last = *stack_b;
+	while (last->next)
+		last = last->next;
+	*stack_b = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "rb\n", 3);
+}
+
+static void	rotate_stack(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	last = *stack;
+	while (last->next)
+		last = last->next;
+	*stack = first->next;
+	first->next = NULL;
+	last->next = first;
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	rotate_stack(stack_a);
+	rotate_stack(stack_b);
+	write(1, "rr\n", 3);
+}
