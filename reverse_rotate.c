3175941307db669ce7dec17042489545db42fd1e
Simon La Grange <sla-gran@fu-r9-p1.s19.be>
2025-12-31 12:56:53 +0100

    feat:rrotate

---
+++ reverse_rotate.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/31 12:55:48 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/12/31 12:55:48 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rra(t_list **stack_a)
+{
+	t_list	*last;
+	t_list	*second_last;
+
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	last = *stack_a;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack_a;
+	*stack_a = last;
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_list **stack_b)
+{
+	t_list	*last;
+	t_list	*second_last;
+
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	last = *stack_b;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack_b;
+	*stack_b = last;
+	write(1, "rrb\n", 4);
+}
+
+static void	reverse_rotate_helper(t_list **stack)
+{
+	t_list	*last;
+	t_list	*second_last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last = *stack;
+	second_last = NULL;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack;
+	*stack = last;
+}
+
+void	rrr(t_list **stack_a, t_list **stack_b)
+{
+	reverse_rotate_helper(stack_a);
+	reverse_rotate_helper(stack_b);
+	write(1, "rrr\n", 4);
+}
