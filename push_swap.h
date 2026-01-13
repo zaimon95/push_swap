d2b800e5ded52bed8802e4eb3a0de5e1c7fd2965
Simon La Grange <sla-gran@shi-r3-p5.s19.be>
2026-01-13 14:30:06 +0100

    feat:parsing structure && pseudocode

--- push_swap.h
+++ push_swap.h
@@ -13,8 +13,24 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft/ft_printf.h""
+# include "libft/ft_printf.h"
+# include "libft/libft/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <limits.h>
 
+void	sa(t_list *stack_a);
+void	sb(t_list *stack_b);
+void	ss(t_list *stack_a, t_list *stack_b);
+void	pa(t_list *stack_a, t_list *stack_b);
+void	pb(t_list *stack_a, t_list *stack_b);
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
+int		is_sign(char c);
+void	check_is_number(char *str);
+
+#endif
