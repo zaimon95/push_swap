ac2e89fda9188367f97d4ebb7518397f7b2e3fb6
Simon La Grange <sla-gran@shi-r3-p5.s19.be>
2026-01-14 11:15:37 +0100

    feat: parsing et check_args

--- parsing.c
+++ parsing.c
@@ -36,34 +36,59 @@
 
 #include "push_swap.h"
 
-int	is_sign(char c)
+static int	ft_contains(int num, char **argv, int i)
 {
-	return (c == '+' || c == '-');
-}
-
-void	check_is_number(char *str)
-{
-	int	i;
-	int	ok;
-
-	i = 0;
-	ok = 1;
-	while ((is_sign(str[0]) && ft_isdigit(i + 1)) || ft_isdigit(i))
+	i++;
+	while (argv[i])
 	{
-		ok = 0;
+		if (ft_atoi(argv[i]) == num)
+			return (1);
 		i++;
 	}
-	if (ok == 0)
+	return (0);
+}
+
+static int	ft_isnum(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (num[0] == '-')
+		i++;
+	while (num[i])
 	{
-		while (str[i])
-		{
-			ft_atoi(str);
-			//malloc et add_back
-		}
+		if (!ft_isdigit(num[i]))
+			return (0);
+		i++;
 	}
+	return (1);
+}
+
+void	ft_check_args(int argc, char **argv)
+{
+	int		i;
+	long	tmp;
+	char	**args;	
+
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
 	else
 	{
-		ft_printf("Erreur");
-		return ;
+		i = 1;
+		args = argv;
 	}
+	while (args[i])
+	{
+		tmp = ft_atoi(args[i]);
+		if (!ft_isnum(args[i]))
+			ft_error("Error");
+		if (ft_contains(tmp, args, i))
+			ft_error("Error");
+		if (tmp < -2147483648 || tmp > 2147483647)
+			ft_error("Error");
+		i++;
+	}
+	if (argc == 2)
+		ft_free(args);
 }
