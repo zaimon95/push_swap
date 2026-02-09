3c8d075cf817f942ea0fcc3de2f27813556758ac
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-09 14:31:40 +0100

    fix : supression fichier inutile

--- src/main.c
+++ src/main.c
@@ -52,11 +52,11 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		return (-1);
-	ft_check_args(argc, argv); //changer pour adapter a mon code
 	stack_a = (t_list **)malloc(sizeof(t_list));
 	stack_b = (t_list **)malloc(sizeof(t_list));
 	*stack_a = NULL;
 	*stack_b = NULL;
+	parse_args(argc, argv, stack_a);
 	init_stack(stack_a, argc, argv);
 	if (is_sorted(stack_a))
 	{
